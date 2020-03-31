b1d344f623c2f08c6b47d6fd2f99566f1db05f2f
tbharathchandra <t.bharathchandra@gmail.com>
2020-03-31 19:08:45 +0530

    Updated pdfcli

--- pdfcli.cpp
+++ pdfcli.cpp
@@ -5,13 +5,40 @@
 
 #include "CairoPDF.h"
 
+void help(){
+    std::cout<<"\nUsage of PDF-CLI\n\n"
+             <<"$(path)/fileName.pdf width height title rectangle x y w h\n"
+             <<"$(path)/fileName.pdf width height title cude x y a\n"
+             <<"$(path)/fileName.pdf width height title circle x y r\n"
+             <<"$(path)/fileName.pdf width height title polygon x y $(anyPath)/points\n"
+             <<"$(path)/fileName.pdf width height title polygon x y text size\n\n";
+}
+
 int main(int argc, char **argv) 
 {
-    // name width height title_with_no_spaces rectangle x y w h
-    // name width height title_with_no_spaces cude x y a
-    // name width height title_with_no_spaces circle x y r
+    // $(path)/fileName.pdf width height title rectangle x y w h
+    // $(path)/fileName.pdf width height title cude x y a
+    // $(path)/fileName.pdf width height title circle x y r
+    // $(path)/fileName.pdf width height title polygon x y $(anyPath)/points
 
-    chdir(getenv("HOME"));
+    if(chdir(getenv("HOME"))!=0){
+        std::cout<<"Problem with changing to home directory\n";
+        exit(0);
+    }
+
+    if(argc==2){
+        if(strcmp("-h",argv[1])==0){
+            help();
+            exit(0);
+        }
+    }
+    
+    if(argc<9){
+        std::cout<<"Enter valid commands. For help use -h as first argument \n";
+        help();
+        exit(0);
+    }
+
 
     char* shape = argv[5];
     char* fileName = argv[1];
@@ -46,8 +73,18 @@ int main(int argc, char **argv)
         double y = atof(argv[7]);
         char* points = argv[8];
         std::ifstream fin(points);
+        if(!fin){
+            std::cout<<"\n Unable to open: "<<fileName<<"\n";
+            exit(0);
+        }
         cpdf.drawPolygon(fin,x,y);
         fin.close();
+    }else if(strcmp(shape,"text")==0){
+        double x = atof(argv[6]);
+        double y = atof(argv[7]);
+        char* text = argv[8];
+        double size = atof(argv[9]);
+        cpdf.drawText(text,x,y,size);
     }
 
     return 0;
